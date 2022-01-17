390ab1f6a60f2cd9956331e464e335e8d9d835c8
Sebastien Dethyre <sebastien.dethyre@etu.univ-amu.fr>
2022-01-17 17:34:43 +0100

    Premiere implémentation

---
+++ Plagiarism/HammingStrategy.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class HammingStrategy implements EditDistanceStrategy {
+
+    /**
+     * Default constructor
+     */
+    public HammingStrategy() {
+    }
+
+    /**
+     * @param String t1 
+     * @param String t2 
+     * @return
+     */
+    public Double computeDistance(void String t1, void String t2) {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @param String t1 
+     * @param String t2 
+     * @return
+     */
+    public Double computeDistance(void String t1, void String t2) {
+        // TODO implement here
+        return null;
+    }
+
+}
