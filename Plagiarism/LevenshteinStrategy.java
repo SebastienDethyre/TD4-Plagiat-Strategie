38d721686e3aff08a330449415d308288daa7f6f
Sebastien Dethyre <sebastien.dethyre@etu.univ-amu.fr>
2022-01-30 18:07:04 +0100

    précisions indiquées

--- Plagiarism/LevenshteinStrategy.java
+++ Plagiarism/LevenshteinStrategy.java
@@ -1,29 +1,13 @@
-
 import java.util.*;
 
-/**
- * 
- */
 public class LevenshteinStrategy implements EditDistanceStrategy {
 
-    /**
-     * Default constructor
-     */
     public LevenshteinStrategy() {
     }
-
-    /**
-     * @param String t1 
-     * @param String t2 
-     * @return
-     */
     
-
-	@Override
+    @Override
 	public Double computeDistance(String t1) {
 		CorpusText c = new CorpusText();
 		return (double) t1.compareToIgnoreCase(c.getText(1));
 	}
-
-
 }
--- Plagiarism/PlagiarismChecker.java
+++ Plagiarism/PlagiarismChecker.java
@@ -1,36 +1,23 @@
-
 import java.util.*;
 
-/**
- * 
- */
 public class PlagiarismChecker {
-
 	private int treshold;
     private EditDistanceStrategy mesStrat;
-    private String t;
+
     public PlagiarismChecker() {
     }
 
-    
     public void setTreshold(int treshold) {
     	this.treshold = treshold;
     }
-    /**
-     * @param checkerStategy
-     */
+
     public void setCheckerStrategy(EditDistanceStrategy checkerStategy) {
         mesStrat = checkerStategy;
     }
 
-    /**
-     * @param String t 
-     * @return boolean
-     */
     public boolean checkPlagiarism(String t) {
         if(mesStrat.computeDistance(t) < treshold)
         	return false;
         return true;
     }
-
 }
