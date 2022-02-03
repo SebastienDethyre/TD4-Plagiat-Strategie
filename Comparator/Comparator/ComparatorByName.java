786625201b2c0b900b51996ea79250f7feb57295
Sebastien Dethyre <sebastien.dethyre@etu.univ-amu.fr>
2022-02-03 19:41:45 +0100

    tentaive infructueuse d'implémenter totalement la classe Promotion

--- Comparator/Comparator/ComparatorByName.java
+++ Comparator/Comparator/ComparatorByName.java
@@ -2,6 +2,6 @@ package Comparator;
 
 public class ComparatorByName {
 	public int compare(Etudiant etud1, Etudiant etud2) {
-		return etud1.getNom().compareTo(etud2.getNom()) +etud1.getPrenom().compareTo(etud2.getPrenom());
+		return etud1.getNom().compareTo(etud2.getNom()) + etud1.getPrenom().compareTo(etud2.getPrenom());
 	}
 }
--- Comparator/Comparator/Promotion.java
+++ Comparator/Comparator/Promotion.java
@@ -1,9 +1,46 @@
 package Comparator;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Plagiarism.EditDistanceStrategy;
+import fr.univamu.iut.exo3.Composant;
+
 public class Promotion {
-	private Etudiant[] Etud;
+	//private Etudiant[] Etud;
+	private List<Etudiant> etud = new ArrayList<>();
+	private Comparator myComp;
+	private int treshold=0;
+   
+
+
+    public void setTreshold(int treshold) {
+    	this.treshold = treshold;
+    }
 	
-	public void triParId() {
-		
+	public Etudiant add(Etudiant e) {
+		etud.add(e);
+		return e;		
 	}
+	
+	public Etudiant remove(Etudiant e) {
+		etud.remove(e);
+		return e;		
+	}
+	
+	public Etudiant getEtudiant(int index) {
+		return etud.get(index);
+	}
+	public void setStrategy(Comparator c) {
+        myComp = c;
+    }
+
+    public void execute(Etudiant etud1, Etudiant etud2) {
+    	for (int i=0;i<etud.size();++i)
+    	{
+    	if(myComp.compare(etud.get(i), etud.get(i+1)) < treshold)
+        	etud.swap(etud, etud.get(i), etud.get(i+1));
+        return true;
+    	}
+    }
 }
